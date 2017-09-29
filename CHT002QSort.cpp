42fcf40851a9183018b246083b2c0305369e3fa2
TheMOK <ilyamokka@gmail.com>
2017-09-29 08:02:54 +0300

    It is not working

--- CHT002QSort.cpp
+++ CHT002QSort.cpp
@@ -4,29 +4,121 @@
 #include <time.h>
 #include <txlib.h>
 
-int const ARR_LENGHT = 700, DBH = 50, DBW = 50, DH = 500, DW = 700;
+int const BUTTONS_NUMBER = 1, ARR_LENGTH = 100, BORDER_HEIGHT = 50, BORDER_WIDTH = 50, ARR_HEIGHT = 500, ARR_WIDTH = 1000;
 
-int binaryFind(int sortedArr[], int startN, int endN, int x);
-void ROR(int arr[], int startN, int endN);
+void stickThread(void* param){
+    int arr[ARR_LENGTH] = {1};
+    (*(((void(*)(int[], int, bool)
+    )
+    *)param
+    ))();
+}
+
+struct Branch {
+    int number, parent, smallChild, bigChild, repeats;
+};
+
+class Button {
+    private:
+    int _x, _y, _width, _height;
+    void(*_sort)(int arr[], int length, bool show);
+    public:
+    Button (void(*sort)(int[], int, bool), int x, int y, int width, int height) {
+        _sort = sort;
+        _x = x;
+        _y = y;
+        _width = width;
+        _height = height;
+    }
+
+    void draw() {
+        txSetFillColor(TX_GREEN);
+        txRectangle(_x, _y, _x + _width, _y + _height);
+    }
+
+    bool click(int x, int y) {
+        if (_x < x && _y < y && _x + _width > x && _y + _height > y) _beginthread(stickThread, 0, &_sort);
+    }
+};
+
+int binaryFind(int sortedArr[], int leftBorderIn, int rightBorderIn, int x);
+void ROR(int arr[], int leftBorderIn, int rightBorderIn);
 void binarySort(int arr[], int lenght);
 void printArr(int arr[]);
-void printArr(int arr[], int l, int r, int o);
+void printArr(Branch tree[]);
+void printArr(int arr[], int left, int right, int bearing);
 void randomFill(int arr[]);
-void mswap(int arr[], int l, int r);
-void mswap(int arr[], int l, int r, int o);
+void mswap(int arr[], int left, int right);
+void mswap(int arr[], int left, int right, int bearing);
 void qwickSort(int arr[], int zero, int lenght);
+void buildTree(int arr[], Branch tree[], int lenght);
+void huskTree(int arr[], Branch tree[], int* nextArrCell, int currentBranch, int lenght);
+void treeSort(int arr[], Branch tree[], int lenght);
+void shakerSort(int arr[], int lenght, bool show);
 
 int main () {
     srand(time(NULL));
-    int arr[ARR_LENGHT] = {};
-    txCreateWindow(2 * DBW + DW, 2 * DBH + DH);
-    randomFill(arr);
-    printArr(arr, -1, -1, -1);
-    txSleep(500);
-    qwickSort(arr, 0, ARR_LENGHT);
-    //binarySort(arr, ARR_LENGHT);
-    printArr(arr, -1, -1, -1);
+    Button buttons[BUTTONS_NUMBER] = {*new Button(&shakerSort, BORDER_WIDTH, BORDER_HEIGHT, 100, 100)};
+    txCreateWindow(2 * BORDER_WIDTH + ARR_WIDTH, 2 * BORDER_HEIGHT + ARR_HEIGHT);
+    for (int i = 0; i < BUTTONS_NUMBER; i++) {
+        buttons[i].draw();
+    }
+    while(true){
+        if (txMouseButtons() == 1) {
+            for (int i = 0; i < BUTTONS_NUMBER; i++) {
+                buttons[i].click(txMouseX(), txMouseY());
+            }
+        }
+    }
     return 0;
+}
+
+void treeSort(int arr[], Branch tree[], int lenght) {
+    buildTree(arr, tree, lenght);
+    int nextArrCell = 0;
+    huskTree(arr, tree, &nextArrCell, 0, lenght);
+}
+
+void huskTree(int arr[], Branch tree[], int* nextArrCell, int currentBranch, int lenght) {
+    if (currentBranch >= lenght || currentBranch < 0) return;
+    huskTree(arr, tree, nextArrCell, tree[currentBranch].smallChild, lenght);
+    arr[*nextArrCell] = tree[currentBranch].number;
+    (*nextArrCell)++;
+    huskTree(arr, tree, nextArrCell, tree[currentBranch].bigChild, lenght);
+}
+
+void buildTree(int arr[], Branch tree[], int lenght) {
+    tree[0] = {arr[0], -1, -1, -1, 1};
+    int nextBranchToFill = 1;
+    for(int i = 1; i < lenght; i++) {
+        int currentBranch = 0;
+        while (true) {
+            if (tree[currentBranch].number == arr[i]) {
+                tree[currentBranch].repeats++;
+                break;
+            }
+            if (tree[currentBranch].number > arr[i]) {
+                if (tree[currentBranch].smallChild > 0) {
+                    currentBranch = tree[currentBranch].smallChild;
+                    continue;
+                } else {
+                    tree[nextBranchToFill] = {arr[i], currentBranch, -1, -1, 1};
+                    tree[currentBranch].smallChild = nextBranchToFill;
+                    nextBranchToFill++;
+                }
+            }
+            if (tree[currentBranch].number < arr[i]) {
+                if (tree[currentBranch].bigChild > 0) {
+                    currentBranch = tree[currentBranch].bigChild;
+                    continue;
+                } else {
+                    tree[nextBranchToFill] = {arr[i], currentBranch, -1, -1, 1};
+                    tree[currentBranch].bigChild = nextBranchToFill;
+                    nextBranchToFill++;
+                }
+            }
+        }
+    }
 }
 
 void binarySort(int arr[], int lenght) {
@@ -35,26 +127,26 @@ void binarySort(int arr[], int lenght) {
     }
 }
 
-void ROR(int arr[], int startN, int endN) {
-    int fict = arr[endN];
-    for(int i = endN - 1; i >= startN; i--) arr[i + 1] = arr[i];
-    arr[startN] = fict;
+void ROR(int arr[], int leftBorderIn, int rightBorderIn) {
+    int fict = arr[rightBorderIn];
+    for(int i = rightBorderIn - 1; i >= leftBorderIn; i--) arr[i + 1] = arr[i];
+    arr[leftBorderIn] = fict;
 }
 
-int binaryFind(int arr[], int startN, int endN, int x) {
-    int r = endN, l = startN;
-    while(r - l > 3) {
-        int m = floor((r + l) / 2);
-        if(arr[m] > x) r = m;
-        else l = m;
+int binaryFind(int arr[], int leftBorderIn, int rightBorderIn, int x) {
+    int right = rightBorderIn, left = leftBorderIn;
+    while(right - left > 3) {
+        int m = floor((right + left) / 2);
+        if(arr[m] > x) right = m;
+        else left = m;
     }
-    for (int i = l; i < r; i++) if (arr[i] > x) return i;
-    return r;
+    for (int i = left; i < right; i++) if (arr[i] > x) return i;
+    return right;
 }
 
 void printArr(int arr[]) {
-//    for(int i = 0; i < ARR_LENGHT; i++) printf("%d\n", arr[i]);
-    for(int i = 0; i < ARR_LENGHT; i++) {
+//    for (int i = 0; i < ARR_LENGTH; i++) printf("%d\n", arr[i]);
+    for (int i = 0; i < ARR_LENGTH; i++) {
         printf("-");
         for(int j = 0; j < arr[i]; j++) printf(".");
         printf("\n");
@@ -62,45 +154,75 @@ void printArr(int arr[]) {
     printf("\n");
 }
 
-void printArr(int arr[], int l, int r, int o) {
+void printArr(Branch tree[]) {
+    for (int i = 0; i < ARR_LENGTH; i++) printf("%d %d %d\n", tree[i].number, tree[i].smallChild, tree[i].bigChild);
+    printf("\n");
+}
+
+void printArr(int arr[], int left, int right, int bearing) {
     txBegin();
     txSetFillColor(RGB(0, 0, 0));
     txClear();
-    for(int i = 0; i < ARR_LENGHT; i++) {
+    for(int i = 0; i < ARR_LENGTH; i++) {
         txSetFillColor(RGB(255, 255, 255));
-        if (i == l || i == r) txSetFillColor(RGB(255, 0, 0));
-        if (i == o) txSetFillColor(RGB(0, 255, 0));
-        txRectangle(DBW + i * DW / ARR_LENGHT, DH + DBH, DBW + i * DW / ARR_LENGHT + DW / ARR_LENGHT, DBH + arr[i] * DH / ARR_LENGHT);
+        if (i == left || i == right) txSetFillColor(RGB(255, 0, 0));
+        if (i == bearing) txSetFillColor(RGB(0, 255, 0));
+        txRectangle(BORDER_WIDTH + i * ARR_WIDTH / ARR_LENGTH, ARR_HEIGHT + BORDER_HEIGHT, BORDER_WIDTH + i * ARR_WIDTH / ARR_LENGTH + ARR_WIDTH / ARR_LENGTH, BORDER_HEIGHT + arr[i] * ARR_HEIGHT / ARR_LENGTH);
     }
     txEnd();
 }
 
 void randomFill(int arr[]) {
-    for(int i = 0; i < ARR_LENGHT; i++) arr[i] = i;
-    for(int i = 0; i < ARR_LENGHT; i++) mswap(arr, i, rand() % ARR_LENGHT);
+    for(int i = 0; i < ARR_LENGTH; i++) arr[i] = i + 1;
+    for(int i = 0; i < ARR_LENGTH; i++) mswap(arr, i, rand() % ARR_LENGTH);
+    //for(int i = 0; i < ARR_LENGTH; i++) arr[i] = rand() % ARR_LENGTH;
 }
 
-void qwickSort(int arr[], int leftIn, int rightIn) {
-    int l = leftIn, r = rightIn, x = arr[l], xn = l;
-    while (r > l) {
-        if (arr[r] > x) r--;
-        if (arr[l] < x) l++;
-        if (arr[r] <= x && arr[l] >= x) mswap(arr, l, r, xn);
+void qwickSort(int arr[], int leftBorderIn, int rightBorderIn) {
+    int left = leftBorderIn, right = rightBorderIn, x = arr[left], xn = left;
+    while (right > left) {
+        if (arr[right] > x) right--;
+        if (arr[left] < x) left++;
+        if (arr[right] <= x && arr[left] >= x) {
+            mswap(arr, left, right, xn);
+            if (arr[left] == arr[right]) right--;
+        }
     }
-    if (rightIn - leftIn <= 1) return;
-    qwickSort(arr, leftIn, l);
-    qwickSort(arr, l + 1, rightIn);
+    if (rightBorderIn - leftBorderIn <= 5) return;
+    qwickSort(arr, leftBorderIn, left);
+    qwickSort(arr, left + 1, rightBorderIn);
 }
 
-void mswap(int arr[], int l, int r) {
-    int fict = arr[l];
-    arr[l] = arr[r];
-    arr[r] = fict;
+void shakerSort(int arr[], int lenght, bool show) {
+    bool retry = false;
+    do {
+        retry = false;
+        for(int i = 0; i < lenght - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                if (show) mswap(arr, i, i + 1, -1);
+                else mswap(arr, i, i + 1);
+                retry = true;
+            }
+        }
+        for(int i = lenght; i > 1; i--) {
+            if (arr[i] < arr[i - 1]) {
+                if (show) mswap(arr, i, i - 1, -1);
+                else mswap(arr, i, i - 1);
+                retry = true;
+            }
+        }
+    } while(retry);
 }
 
-void mswap(int arr[], int l, int r, int o) {
-    int fict = arr[l];
-    arr[l] = arr[r];
-    arr[r] = fict;
-    printArr(arr, l, r, o);
+void mswap(int arr[], int left, int right) {
+    int fict = arr[left];
+    arr[left] = arr[right];
+    arr[right] = fict;
+}
+
+void mswap(int arr[], int left, int right, int bearing) {
+    int fict = arr[left];
+    arr[left] = arr[right];
+    arr[right] = fict;
+    printArr(arr, left, right, bearing);
 }
