327720841d03e56a21b743363110c9b271099d96
TheMOK <ilyamokka@gmail.com>
2017-11-03 10:03:41 +0300

    Update CHT004Singleton.cpp

--- CHT004Singleton.cpp
+++ CHT004Singleton.cpp
@@ -19,3 +19,27 @@ int main () {
     while(Singleton::getInstance().a < 20) printf("%d\n", ++Singleton::getInstance().a);
     return 0;
 }
+/*
+#include <txLib.h>
+
+int findStr(char arr[], char str[]);
+
+int main() {
+    printf("%d", findStr("mememeow", "meow"));
+}
+
+int findStr(char arr[], char str[]) {
+    int arrLen = strlen(arr), strLen = strlen(str);
+
+    for(int i = 0, j = 0; i < arrLen; i++) {
+        if(arr[i] == str[j]) j++;
+        else {
+            j = 0;
+        }
+        if(j == strLen) return i - strLen;
+    }
+
+    return -1;
+}
+
+*/
